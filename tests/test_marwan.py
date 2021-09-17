46cd36602ee8844b1363f49ccc738a8537ffaffe
Marwan Hawari <marwan.hawari@gmail.com>
2021-09-17 00:14:55 -0700

    first commit

---
+++ tests/test_marwan.py
@@ -0,0 +1,15 @@
+import marwan
+
+
+def test_main(capsys):
+    marwan.main(["-lpwg"])
+
+    out, err = capsys.readouterr()
+    assert (
+        out
+        == """Opening https://www.github.com/marwanhawari/
+Opening https://www.linkedin.com/in/marwanhawari/
+Opening https://pypi.org/user/marwanhawari/
+Opening https://www.marwanhawari.com/\n"""
+    )
+    assert err == ""
